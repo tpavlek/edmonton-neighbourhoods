974e7806c1344d244afedad853225eedae7511c0
Troy Pavlek <troy@tpavlek.me>
2016-12-15 14:28:07 -0700

    yields social meta

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>@yield('title')</title>
+    @yield('social_meta', '')
     <link rel="stylesheet" href="/css/app.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
